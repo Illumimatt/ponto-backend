e199f3f5ce6aceb9f73df3b767483a2d0442b943
Illumimatt <deters.mk@sempreceub.com>
2024-11-12 10:28:49 -0300

    teste em sala 12/11

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ const app = express();
 const PORT = 3000;
 
 const sequelize = require('./config/db');
+const Usuario = require('./models/usuario');
+const Ponto = require('./models/ponto');
 
-sequelize.authenticate()
+
+sequelize.sync({ alter: true })
 .then(() => {
-    console.log("conexão sucesso!");
-}).catch(error => {
-    console.log("erro");
+    console.log("BD sincronizado");
+})
+.catch(error => {
+    console.log("Erro!");
 });
+
+
+app.get('/usuario/:id_usuario', async (req, res) => {
+    const usuario= await Usuario.findAll({
+        where: {
+            id_usuario: req.params.id_usuario,
+        },
+    });
+    res.json(usuario);
+});
+
+app.post('/usuario', async (req, res) => {
+   
+    console.log(req.body);
+    const usuario = await Usuario.create({
+        nome: req.body.nome, 
+        email: req.body.email, 
+        senha: req.body.senha, 
+        login: req.body.login, 
+        permissao: req.body.permissao,
+    })
+    res.status(201).json(usuario);
+    // nome, email, senha, login, permissao("usuario""administrador")
+})
 
 // ROTAS
 app.get('/', (req, res) => {
